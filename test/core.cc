f227ea1801cfc8a272c8dbfb909baad278d84410
ZbrDeev <github.cannot978@passinbox.com>
2025-01-05 23:01:25 +0100

    Add unit test

---
+++ test/core.cc
@@ -0,0 +1,23 @@
+#include "include/core/request.hh"
+#include "include/utils/http_version.hh"
+#include "include/utils/method.hh"
+#include <gtest/gtest.h>
+#include <string>
+
+TEST(CoreTest, RequestTest) {
+  std::string request =
+      "POST /submit-form HTTP/1.1\r\nHost: www.example.com\r\nContent-Type: "
+      "application/x-www-form-urlencoded\r\nContent-Length: "
+      "27\r\n\r\nname=JohnDoe&age=30&city=Paris";
+
+  HttpRequest req(request);
+
+  ASSERT_EQ(req.method, Method::POST);
+  ASSERT_EQ(req.uri, "/submit-form");
+  ASSERT_EQ(req.version, HttpVersion::HTTP1);
+  ASSERT_EQ(req.body, "name=JohnDoe&age=30&city=Paris");
+
+  ASSERT_EQ(req.headers["Host"], "www.example.com");
+  ASSERT_EQ(req.headers["Content-Type"], "application/x-www-form-urlencoded");
+  ASSERT_EQ(req.headers["Content-Length"], "27");
+}
