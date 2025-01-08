3e366d222ff2e4baa33006baa12d08e4dc46d0ef
ZbrDeev <github.cannot978@passinbox.com>
2025-01-08 14:40:04 +0100

    Add server handler

--- include/server.hh
+++ include/server.hh
@@ -2,33 +2,45 @@
 #define NETCPP_SERVER_HH
 
 #include "include/posix/descriptor.hh"
+#include <memory>
 #include <string>
 
 #if defined(__linux__)
 #include <sys/epoll.h>
 #endif
 
+class ServerHandler {
+public:
+  ServerHandler();
+  virtual void onTcpHandshake() = 0;
+  virtual void onRequest() = 0;
+};
+
 class Server {
 public:
   Server(const std::string address, const unsigned int port);
-  
+  inline void setHandler(std::unique_ptr<ServerHandler> handler) {
+    handler_ = std::move(handler);
+  }
+
   inline void run() {
-    #if defined(__linux__)
-        runWithLinux();
-    #endif
+#if defined(__linux__)
+    runWithLinux();
+#endif
   }
 
 private:
   std::string address_;
   unsigned int port_;
+  std::unique_ptr<ServerHandler> handler_;
 
-  #if defined(__linux__)
-    int serverFd;
-    int epollFd;
-    epoll_event events[MAX_EVENT];
+#if defined(__linux__)
+  int serverFd;
+  int epollFd;
+  epoll_event events[MAX_EVENT];
 
-    void runWithLinux();
-  #endif
+  void runWithLinux();
+#endif
 };
 
 #endif // NETCPP_SERVER_HH
