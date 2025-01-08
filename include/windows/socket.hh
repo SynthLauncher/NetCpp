bbc1bb1fb584378acb38a358a4f9c7034c4efb0f
STierProgrammer <stierprogrammer@gmail.com>
2025-01-08 19:17:35 +0400

    Fixed stuff connected to windows socket server

--- include/windows/socket.hh
+++ include/windows/socket.hh
@@ -6,16 +6,12 @@
 /*
     The UNICODE macro is a preprocessor directive in Windows programming that enables the use of wide-character strings (wchar_t) and corresponding Unicode APIs, instead of the standard ASCII-based or multi-byte character set (MBCS) versions of functions.
 */
-#ifdef UNICODE
-    #undef UNICODE
-#endif // UNICODE
+#undef UNICODE
 
 /*
     WIN32_LEAN_AND_MEAN is a preprocessor directive used to reduce the size and compilation time of Windows headers by excluding rarely used components.
 */
-#ifndef WIN32_LEAN_AND_MEAN
-    #define WIN32_LEAN_AND_MEAN
-#endif // WIN32_LEAN_AND_MEAN
+#define WIN32_LEAN_AND_MEAN
 
 #include <windows.h>
 #include <winsock2.h>
