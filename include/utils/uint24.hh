60c605a5614c9377c71cd381753bfa1fe4b60fed
ZbrDeev <github.cannot978@passinbox.com>
2025-01-21 17:09:02 +0100

    Add operator& and operator== in uint24

--- include/utils/uint24.hh
+++ include/utils/uint24.hh
@@ -6,9 +6,19 @@ struct uint24 {
 
   uint24() {}
   uint24(unsigned int data) : data{data} {}
-  inline uint24 &operator+=(int sn) {
+
+  inline uint24 &operator+=(const int sn) {
     data += sn;
     return *this;
+  }
+
+  inline friend bool operator==(const uint24 &lhs, const uint24 &rhs) {
+    return lhs.data == rhs.data;
+  }
+
+  template <typename T>
+  inline bool operator&(const T &sn) {
+    return data & sn;
   }
 } __attribute__((packed));
 
